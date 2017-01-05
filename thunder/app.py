cb03dc9d25c0db18091c22cea95505475c0993d4
wikibootup <wikibootup@gmail.com>
2017-01-05 15:49:19 +0900

    deploy app in /thunder/

--- thunder/app.py
+++ thunder/app.py
@@ -1,11 +1,10 @@
 from flask import Flask
 
-import os
-
-
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return 'OK!';
+
 if __name__ == '__main__':
-    app.debug = True
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
